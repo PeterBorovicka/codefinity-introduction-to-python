d87f8e4d96ca6d1f6fceb14fece1dbb361948136
PeterBorovicka <>
2026-02-08 12:43:31 +0000

    String manipulation

--- variables_and_types/string_manipulation/main.py
+++ variables_and_types/string_manipulation/main.py
@@ -2,4 +2,4 @@ grocery_items = "milk,cheese,bread,apples,oranges,chicken"
 print("fruits available:", grocery_items[18:32])
 non_fruit1 = "milk"
 non_fruit2 = "cheese"
-print(f"list_of_non-fruits: {non_fruit1},{non_fruit2}")
+print("list_of_non-fruits: " + non_fruit1 + "," + non_fruit2)
