b811f412697dff4c87660604fca0090696998410
PeterBorovicka <>
2026-05-18 07:17:14 +0000

    Dictionaries

--- other_data_types/dictionaries/main.py
+++ other_data_types/dictionaries/main.py
@@ -5,5 +5,19 @@ grocery_inventory = {
     "Bread":  (117, "Bakery"),
     "Apples": (141, "Produce")
 }
+#Print the details of "Bread": Details of Bread: <$bread_details>.
+bread_details = grocery_inventory.get("Bread")
+print("Details of Bread:", bread_details)
 
-print(grocery_inventory)
+#After adding "Cookies", print the updated inventory: Inventory after adding Cookies: <$grocery_inventory>.
+grocery_inventory["Cookies"] = (143, "Bakery")
+print("Inventory after adding Cookies:", grocery_inventory)
+
+#After removing "Eggs", print the updated inventory: Inventory after removing Eggs: <$grocery_inventory>.
+grocery_inventory.pop("Eggs")
+print("Inventory after removing Eggs:", grocery_inventory)
+
+
+
+
+
