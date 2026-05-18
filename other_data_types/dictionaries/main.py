3b673e80b5c82b663a4098775804b7f5c8d34d5f
PeterBorovicka <>
2026-05-18 07:16:51 +0000

    Dictionaries

---
+++ other_data_types/dictionaries/main.py
@@ -0,0 +1,9 @@
+# Define the grocery_inventory dictionary with item details as (ID, category)
+grocery_inventory = {
+    "Milk":   (113, "Dairy"),
+    "Eggs":   (116, "Dairy"),
+    "Bread":  (117, "Bakery"),
+    "Apples": (141, "Produce")
+}
+
+print(grocery_inventory)
