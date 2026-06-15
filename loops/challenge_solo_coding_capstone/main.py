40dadb2d4a2cc8238545157a3ea6551b257e144b
PeterBorovicka <>
2026-06-15 18:11:27 +0000

    Challenge: Solo Coding Capstone

---
+++ loops/challenge_solo_coding_capstone/main.py
@@ -0,0 +1,20 @@
+# 1) Define inventory first
+inventory = {
+    "Bread":  [42, 1.20, 0.99],
+    "Eggs":   [225, 2.12, 1.99],
+    "Apples": [9,  1.50, 1.35]
+}
+# 2) Then start your loop
+for item in inventory:
+    stock = inventory[item][0]
+    regular_price = inventory[item][1]
+    discounted_price = inventory[item][2]
+
+    if stock < 30:
+        print(f"{item} need restocking.")
+    elif stock > 100:
+        print(f"{item} should be sold at the discounted price of {discounted_price}.")
+    else:
+        print(f"{item} should be sold at the regular price of {regular_price}.")
+
+
